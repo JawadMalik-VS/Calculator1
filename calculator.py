c569ea47ca39cf9ab749eb8af8c4760f13b8a7c8
Jawad Malik <jawad.maliki786@gmail.com>
2024-08-09 12:14:34 +0500

    first commit

---
+++ calculator.py
@@ -0,0 +1,14 @@
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+operation = input("Enter operation: ")
+
+if operation == "+":
+    print(number1 + number2)
+if operation =="-":
+     print(number1- number2)
+if  operation== "*":
+     print(number1*number2)
+if operation =="/":
+     print(number1/number2) 
+else:
+     print ("You entered wrong operation")            
